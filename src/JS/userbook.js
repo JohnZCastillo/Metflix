17953883326284d2a35c6a9b82f47d1ab6c2df78
SheromG <david.sherom.granada@gmail.com>
2023-06-30 19:15:13 +0800

    Second commit day four

--- src/JS/userbook.js
+++ src/JS/userbook.js
@@ -6,16 +6,15 @@ fetch('../JS/data.json')
     {
         data.forEach(movie => 
         {
-            const { Name, Description, ImagePath } = movie;
+            const { Number, Name, Description, ImagePath } = movie;
 
             div.insertAdjacentHTML('afterbegin',
             `<div id = "movie" style = "background-image: linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)), url(${ImagePath});"> 
-            <div class = "moviedetails">
-                <h3 class = "moviename">${Name}</h3>
-                <p class = "moviedescription">${Description}</p>
-                <button class = "moviebook">Book</button>
-            </div>
-
+                <div class = "moviedetails">
+                    <h3 class = "moviename">${Name}</h3>
+                    <p class = "moviedescription">${Description}</p>
+                    <button class = "moviebook">Book</button>
+                </div>
             </div>`);
         });
     })
